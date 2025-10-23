15be69b1c472c73a0b33c3f74e45cebd71220a87
26blevine <26blevine@gannacademy.org>
2025-10-23 11:56:20 -0400

    loopidy doop

--- src/municipal_information.py
+++ src/municipal_information.py
@@ -4,7 +4,14 @@ from os import sep
 import pandas as pd
 
 def main():
-    pass 
+    vals = pd.read_csv("src/municipal.tsv", sep="\t")
+    
+    shape = vals.shape
+    print(f"Shape: {shape[0]}, {shape[1]}")
+    print("Columns:")
+    for col in vals.columns:
+        print(col)
+
 
 
 if __name__ == "__main__":
